53613c49c26fa695c6e4df1ac68ab7c9efbc6963
jiangwei <1048851186@qq.com>
2024-09-18 15:19:17 +0800

    xxxx

---
+++ MemoryApp/BaseOneCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  BaseOneCollectionViewCell.swift
+//  MemoryApp
+//
+//  Created by edy on 2024/9/18.
+//
+
+import UIKit
+
+class BaseOneCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
